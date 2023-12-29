fe96fdbaa104227e1017d59cefe0dfb129fb163d
Satyam Anand <satyam.anand@bolt.earth>
2023-12-29 19:16:23 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import './App.css';
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import 'bootstrap';
-import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import Routing from './Components/Routing';
+import "bootstrap/dist/js/bootstrap.js";
+import "./App.css";
+import Routing from "./Components/Routing";
 
 function App() {
-  return (
-    <Routing />
-  );
+    return <Routing />;
 }
 
 export default App;
